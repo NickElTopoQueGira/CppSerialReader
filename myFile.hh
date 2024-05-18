47de53f72d25796915f7c4288b56915bdb526e34
NickElTopoQueGira <niccolosesana@outlook.it>
2024-05-18 23:15:21 +0200

    Bug fix and addition of comments to functions

--- myFile.hh
+++ myFile.hh
@@ -8,9 +8,45 @@
 #include <ctime>
 #include <sstream>
 
-// Functions
-int createFile(char* path);
+/**
+ * Function: createFileName
+ * Input values: 
+ * Output values: filename
+ * Return values: error number
+ * Description: 
+ * Function to create the file name with the date and time of cration. 
+ * in the format: dd/mm/yyy_h:m:s.txt
+ */
 int creteFileName (char* filename);
+
+/**
+ * Function: createFile
+ * Input values: file path
+ * Output values: 
+ * Return values: error number
+ * Description: 
+ * Creates the file at the location specified in input
+ */
+int createFile(char* path);
+
+/**
+ * Function: writeInAFile
+ * Input values: file log path, text to write
+ * Output values:
+ * Return values: error number
+ * Description: 
+ * Adds to the file specified as input the message
+ */
+int writeInAFile(char* path, char* text)
+
+/**
+ * Function: isFileExist
+ * Input values: path
+ * Output values: 
+ * Return values: error number
+ * Description: 
+ * Verify existence of the file.
+ */
 int isFileExist(char* path);
 
 #endif
